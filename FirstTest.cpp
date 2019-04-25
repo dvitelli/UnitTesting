df0b586f2213ac94f24411862cef780cdca36070
Dom <46989387+dvitelli@users.noreply.github.com>
2019-04-25 09:43:24 -0700

    Create FirstTest.cpp

---
+++ FirstTest.cpp
@@ -0,0 +1,15 @@
+// CheckoutTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <gtest/gtest.h>
+#include "Checkout.h"
+
+TEST(CheckoutTests, CanInstantiateCheckout) {
+
+	Checkout co;
+
+}
+
+
